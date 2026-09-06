e0c36d2d77997b4ad9240315b948bb2b42742e3f
Jack Sutera <jsutera@binghamton.edu>
2026-09-06 19:33:06 -0400

    started prj1

---
+++ submit/prj1-sol/make.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+
+# TODO: use this script to compile any artifacts needed by your parser,
+# assuming that the current directory is the one containing this file.
+
+echo "no build done"
+
+
---
+++ submit/prj1-sol/run.sh
@@ -0,0 +1,10 @@
+#!/bin/sh
+
+# TODO: set up this file to run your parser assuming that the current
+# directory is the one containing this file.
+
+
+# No submitted files will be executable; ensure that this script works
+# under those conditions.
+
+
