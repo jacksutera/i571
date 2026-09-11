8bf85969f60ebf3ee5f9e9f225a0366fe0ff0ed5
jacksutera <jas9505@gmail.com>
2026-09-11 16:38:31 -0400

    absolute path fix

--- submit/prj1-sol/run.sh
+++ submit/prj1-sol/run.sh
@@ -7,5 +7,5 @@
 # No submitted files will be executable; ensure that this script works
 # under those conditions.
 
-DIR = "$(cd "$(dirname "$0")" && pwd)"
+DIR = $(dirname "$0")
 exec python3 "$DIR/bits-571.py"
