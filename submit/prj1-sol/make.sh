107f11c906afae72bbf62f7a05abca4bf66b7e6e
jacksutera <jas9505@gmail.com>
2026-09-12 14:01:52 -0400

    documentation and TODO removal part 2

--- submit/prj1-sol/make.sh
+++ submit/prj1-sol/make.sh
@@ -1,7 +1,5 @@
 #!/bin/sh
 
-# TODO: use this script to compile any artifacts needed by your parser,
-# assuming that the current directory is the one containing this file.
 #using python, so just exit
 exit 0
 
